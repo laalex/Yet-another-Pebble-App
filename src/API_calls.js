32e1dc6e56dd993cea1a03054acbb14959cf5834
L.A. Alex <roshkattu94@gmail.com>
2014-12-07 17:43:04 +0200

    Updated code
    Updated the code to have more comments and explain it better

--- src/API_calls.js
+++ src/API_calls.js
@@ -1,3 +1,9 @@
+/**
+* All this code is executed on the phone
+* @author: Lamba Alexandru
+* @contact: contact@alexandrulamba.com
+*/
+
 // Location success callback
 function locationSuccessCallback(pos){
   // Build the long/lat coords
@@ -7,45 +13,46 @@ function locationSuccessCallback(pos){
   var current_address;
   var req_url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lng+"&sensor=false";
   // Return the JSON data
-  var request = new XMLHttpRequest();
-  request.open('GET', req_url, true);
-  request.onload = function(e){
-    if (request.readyState == 4 && request.status == 200) {
-      var response = JSON.parse(request.responseText);
+  var request = new XMLHttpRequest(); // Create new AJAX request
+  request.open('GET', req_url, true); // Open connection to the req_url
+  request.onload = function(e){ // Check the AJAX STATE
+    if (request.readyState == 4 && request.status == 200) { // IF ajax finished and response is OK (200)
+      var response = JSON.parse(request.responseText); // Parse the response from the API
       // Build the current address
-      current_address = response.results[0].formatted_address;
-      var dictionary = {
+      current_address = response.results[0].formatted_address; // Get the current address
+      var dictionary = { // Create a dictionary that we send to the pebble app
         "KEY_APISTRING": current_address
       };
+      // Initialise a request to the pebble application
+      // and send our dictionary (the propper data)
       Pebble.sendAppMessage(dictionary, function(){console.log("Location sent");}, function(){console.log("failed");});
     }
   };
+  // Finish tue ajax request
   request.send(null);
-
   
 }
 
 // Location failed callback
-function locationErrorCallback(error){
-  console.log("There was an error getting your location. See below");
-  console.log(error);
+function locationErrorCallback(error){ // Failed to get the location from the phone
+  console.log("There was an error getting your location. See below"); // Log this problem
 }
 
 //Get's the current location
 function getLocation(){
-  navigator.geolocation.getCurrentPosition(
-    locationSuccessCallback,
-    locationErrorCallback,
-    {timeout: 15000, maximumAge: 60000}
+  navigator.geolocation.getCurrentPosition( // Get the location from the phone
+    locationSuccessCallback, // Set callback for success
+    locationErrorCallback, // Set callback for errors
+    {timeout: 15000, maximumAge: 60000} // Set timeout and maximum age for this request
   );
 }
 
 // Add event listener to be ready
-Pebble.addEventListener('ready', function(ev){
-  getLocation();
+Pebble.addEventListener('ready', function(ev){ // Initialise the AppMessage event listener for "ready" state
+  getLocation(); // When we are ready, request the location
 });
 
 // Add event listener for "appmessage"
-Pebble.addEventListener('appmessage',function(ev){
-  getLocation();// This will get the updates later on
+Pebble.addEventListener('appmessage',function(ev){ // If we have any appmessage request from pebble
+  getLocation();// Update the location again and send it to the pebble
 });
--- src/main_activity.c
+++ src/main_activity.c
@@ -1,20 +1,27 @@
+/**
+* All this code is executed on the phone
+* @author: Lamba Alexandru
+* @contact: contact@alexandrulamba.com
+*/
+
 #include <pebble.h>
-#define KEY_APISTRING 0
+#define KEY_APISTRING 0 //The name of the key that is returned by the PebbleJS Kit
   
 // Var declaration
-static Window *main_window;
-static TextLayer *text_layer;
-static TextLayer *api_data_text;
+static Window *main_window; // Create a new window layer
+static TextLayer *text_layer; // Create a new text layer
+static TextLayer *api_data_text; // Create a new text layer
 
-// Window load
+// Window load - handler
 static void main_window_load(Window *window){
-  // Text settings
+  // Create a new text layer and add it's properties
   text_layer = text_layer_create(GRect(0, 0, 144, 28));
   text_layer_set_background_color(text_layer, GColorBlack);
   text_layer_set_text_color(text_layer, GColorWhite);
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   text_layer_set_text(text_layer, "You're at");
   
+  // Create a new text layer and add it's properties
   api_data_text = text_layer_create(GRect(0, 28, 144, 140));
   text_layer_set_background_color(api_data_text, GColorBlack);
   text_layer_set_text_color(api_data_text, GColorWhite);
@@ -29,7 +36,9 @@ static void main_window_load(Window *window){
 
 // Window unload
 static void main_window_unload(Window *window){
+  // Destroy the text layer to free up space
   text_layer_destroy(text_layer);
+  // Destroy the text layer to free up space
   text_layer_destroy(api_data_text);
 }
 
@@ -38,15 +47,16 @@ static void main_window_unload(Window *window){
 static void inbox_received_callback(DictionaryIterator *iterator, void *context){
   Tuple *t = dict_read_first(iterator); // Get first dictionary value
   // Create a new buffer for the location string
-  static char location_buffer[200];
+  static char location_buffer[200]; // Buffer length should be enough to catch the entire string
   // Iterate troughout the dictionary values
   while(t != NULL){
-    switch(t->key){
-      case KEY_APISTRING:
+    switch(t->key){ // Switch the key that we receive
+      case KEY_APISTRING: // If we identify the key
+        // Include the information from the key as a STRING into our buffer
         snprintf(location_buffer, sizeof(location_buffer), "%s", t->value->cstring);
       break;
-      default:
-        APP_LOG(APP_LOG_LEVEL_WARNING, "Key %d is not recognized", (int)t->key);
+      default: // The default case means we haven't indentified the string
+        APP_LOG(APP_LOG_LEVEL_WARNING, "Key %d is not recognized", (int)t->key); // Log the error
       break;
     }
     // Read the next key in the dictionary if available
@@ -54,17 +64,19 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
   }
   // Add the location_buffer to the clock watchface
   APP_LOG(APP_LOG_LEVEL_DEBUG, "The value of location is: %s", location_buffer);
+  // Update the text layer so we can see the address at where we are
   text_layer_set_text(api_data_text, location_buffer);
   
 }
 static void inbox_dropped_callback(AppMessageResult reason, void *context){
-  
+  // TO DO
+  // Handle the dropped inbox message and log the reason
 }
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context){
-  
+  // TO DO
 }
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context){
-  
+  // TO DO
 }
 
 
@@ -74,31 +86,32 @@ static void init(){
   main_window = window_create();
   // Set the handlers
   window_set_window_handlers(main_window, (WindowHandlers){
-    .load = main_window_load,
-    .unload = main_window_unload
+    .load = main_window_load, // Set the handler for window load
+    .unload = main_window_unload // Set the handler for window unload
   });
   
   // Push the window to the stack
   window_stack_push(main_window, true); // Push the window animated
   
   // Init the callbacks for AppMessage API
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
+  app_message_register_inbox_received(inbox_received_callback); // Init callback
+  app_message_register_inbox_dropped(inbox_dropped_callback); // Init callback
+  app_message_register_outbox_failed(outbox_failed_callback); // Init callback
+  app_message_register_outbox_sent(outbox_sent_callback); // Init callback
   
   // Start app message
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
+// De-init the application (destructor)
 static void deinit(){
+  // Destroy the main window to free memory on the Pebble watch
   window_destroy(main_window);
 }
 
-
-
+// Main loop
 int main(void){
-  init();
-  app_event_loop();
-  deinit();
+  init(); // Init everything
+  app_event_loop(); // Loop everything (run app and take actions)
+  deinit(); // When app is leaved, de-init everything to free memory
 }
